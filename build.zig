b87d4e6ea5ae5e35b540123fddbafced51b3a272
HaraldWik <wikhallharald@gmail.com>
2026-06-20 15:27:52 +0200

    Updated build.zig

--- build.zig
+++ build.zig
@@ -1,20 +1,29 @@
 const std = @import("std");
 
 pub fn build(b: *std.Build) void {
-    const target = b.standardTargetOptions(.{});
+    const target = b.standardTargetOptions(.{ .default_target = .{ .os_tag = .freestanding, .cpu_arch = .wasm32 } });
     const optimize = b.standardOptimizeOption(.{});
+
+    const sdk_path = b.path("src/main.zig");
+
+    _ = b.addModule("sdk", .{
+        .root_source_file = sdk_path,
+        .target = target,
+        .optimize = optimize,
+    });
 
     const exe = b.addExecutable(.{
         .name = "mod",
         .root_module = b.createModule(.{
-            .root_source_file = b.path("src/main.zig"),
+            .root_source_file = sdk_path,
             .target = target,
             .optimize = optimize,
-            .imports = &.{
-                // .{ .name = "mod", .module = mod },
-            },
+            .imports = &.{},
+            .pic = true,
         }),
     });
+    exe.entry = .{ .symbol_name = "guest_initialize" }; // make sure guest_initialize is compiled
+    exe.rdynamic = true;
 
     b.installArtifact(exe);
 }
