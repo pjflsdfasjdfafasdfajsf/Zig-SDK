a9c7f7d0418e0eaf9f0d7fa87b93c198364bf733
HaraldWik <wikhallharald@gmail.com>
2026-06-20 15:16:38 +0200

    Initial commit

---
+++ build.zig
@@ -0,0 +1,20 @@
+const std = @import("std");
+
+pub fn build(b: *std.Build) void {
+    const target = b.standardTargetOptions(.{});
+    const optimize = b.standardOptimizeOption(.{});
+
+    const exe = b.addExecutable(.{
+        .name = "mod",
+        .root_module = b.createModule(.{
+            .root_source_file = b.path("src/main.zig"),
+            .target = target,
+            .optimize = optimize,
+            .imports = &.{
+                // .{ .name = "mod", .module = mod },
+            },
+        }),
+    });
+
+    b.installArtifact(exe);
+}
